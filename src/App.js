6950d88e5e94c64112595567c1506c1cb0a07f2e
tihonnazarov <139996570+tihonnazarov@users.noreply.github.com>
2023-07-25 00:47:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+    });
 
-export default App;
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("ФИО:", formData.fullName);
+        console.log("Телефон:", formData.phoneNumber);
+        console.log("Email:", formData.email);
+        alert("Данные успешно отправлены!");
+    };
+
+    return (
+        <form onSubmit={handleSubmit} style={{border: "2px solid pink", padding: "20px", backgroundColor: "black", boxShadow: "0px 14px 28px black"}}>
+            <h1>Регистрация</h1>
+            <div>
+                <label>
+                    <h3>ФИО:</h3>
+                    <input
+                        style={{border: "solid 1px pink"}}
+                        type="text"
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    <h3>Номер телефона:</h3>
+                    <input
+                        style={{border: "solid 1px pink"}}
+                        type="tel"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    <h3>Email адрес:</h3>
+                    <input
+                        style={{border: "solid 1px pink"}}
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+            </div>
+
+            <button type="submit" style={{marginTop: "20px", border: "solid 1px pink"}}>Отправить</button>
+        </form>
+    );
+};
+
+export default RegistrationForm;
